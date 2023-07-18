0f41369da68885c6b48bf15caa7a3c821cbdac67
sugutt <sugutmanu@gmail.com>
2023-07-18 10:28:12 +0300

    new

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,25 @@
+use std::error::Error;
 
- fn main() {
-    run_native(app, native ,options)
- r
+struct Articles{
+    articles: Vec<Article>
+}
+
+struct Article{
+    title: String,
+    description: String,
+    url: String,
+}
+
+fn get_articles(url: &str) -> Result<Articles, Box<dyn Error>> {
+    let response = ureq::get(url).call()?.into_string()?;
+
+    dbg!(response)
+    todo!()
+}
+
+ fn main() {}
+    let url: &str = "https://newsapi.org/v2/top-headlines?country=us&apiKey=cde7cbd805a447c1899a27d8cb493501";
+    let articles: Result<{Unknown},Box<dyn>> = get_articles(url);
+}
+
+
